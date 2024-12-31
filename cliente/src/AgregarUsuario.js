fd7da77ce1350b4a84cc3cf938886f8198aa802b
katiuska.franco <kati.franco@gmail.com>
2024-12-30 21:40:40 -0500

    Initial commit

---
+++ cliente/src/AgregarUsuario.js
@@ -0,0 +1,10 @@
+import React from "react";
+function AgregarUsuario() {
+  return (
+    <div>
+      <h2>Agregar usuario</h2>
+    </div>
+  );
+}
+
+export default AgregarUsuario;
---
+++ cliente/src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import ListaUsuarios from "./ListaUsuarios";
+import AgregarUsuario from "./AgregarUsuario";
+import EditarUsuario from "./EditarUsuario";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <div className="App">
+      <h1>Crud MERN STACK</h1>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ListaUsuarios />} exact></Route>
+          <Route
+            path="/agregarusuario"
+            element={<AgregarUsuario />}
+            exact
+          ></Route>
+          <Route
+            path="/editarusuario"
+            element={<EditarUsuario />}
+            exact
+          ></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
